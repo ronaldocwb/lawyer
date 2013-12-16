d3c371d60f6a1115df7d9c6b3e60989fab97f64a
Deividi Cavarzan <deividi.cavarzan@gmail.com>
2013-12-16 00:12:05 -0200

    agenda

--- lawyer-core/src/main/java/br/com/lawyer/core/util/mail/MailSender.java
+++ lawyer-core/src/main/java/br/com/lawyer/core/util/mail/MailSender.java
@@ -16,35 +16,35 @@ import java.io.Serializable;
 
 @Component
 public class MailSender implements ApplicationListener<MailEvent>, Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private static final Logger logger = Logger.getLogger(MailSender.class);
-	
-	@Override
-	public void onApplicationEvent(MailEvent mailEvent) {
-		sendMail(mailEvent.getMailMessage());
-	}
-	
-	public void sendMail(MailMessage email){
-		try    {
-			Session mailSession = InitialContext.doLookup("java:/LawyerMail");
-			
-			MimeMessage m = new MimeMessage(mailSession);
-			InternetAddress from = new InternetAddress("lawyer@lawyer.com", "Lawyer");
 
-			m.setFrom(from);
-			m.setSubject(email.getAssunto());
-			m.setSentDate(new java.util.Date());
-			m.setContent(email.getMensagem(),"text/html");
-			
-			m.setRecipients(Message.RecipientType.TO, (Address[]) email.getDestinatario().toArray());
-			Transport.send(m);
-			logger.info("Email enviado com Sucesso!");
-		} catch (Exception e){
-			e.printStackTrace();
-			System.out.println("<font color=red>Error in Sending Mail: "+e+"</font>");
-			throw new RuntimeException("Erro ao enviar email");
-		}
-	}
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = Logger.getLogger(MailSender.class);
+
+    @Override
+    public void onApplicationEvent (MailEvent mailEvent) {
+        sendMail(mailEvent.getMailMessage());
+    }
+
+    public void sendMail (MailMessage email) {
+        try {
+            Session mailSession = InitialContext.doLookup("java:/LawyerMail");
+
+            MimeMessage m = new MimeMessage(mailSession);
+            InternetAddress from = new InternetAddress("lawyer@lawyer.com", "Lawyer");
+
+            m.setFrom(from);
+            m.setSubject(email.getAssunto());
+            m.setSentDate(new java.util.Date());
+            m.setContent(email.getMensagem(), "text/html");
+
+            m.setRecipients(Message.RecipientType.TO, (Address[]) email.getDestinatario().toArray());
+            Transport.send(m);
+            logger.info("Email enviado com Sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("<font color=red>Error in Sending Mail: " + e + "</font>");
+            throw new RuntimeException("Erro ao enviar email");
+        }
+    }
 }
